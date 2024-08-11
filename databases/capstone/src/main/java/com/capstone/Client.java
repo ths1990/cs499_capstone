e58ff8380dea2eaf5e564a06122302790a175e55
Ty Simpson <tyler.h.simpson@gmail.com>
2024-08-11 14:56:44 -0400

    Add files via upload

---
+++ databases/capstone/src/main/java/com/capstone/Client.java
@@ -0,0 +1,62 @@
+package com.capstone;
+
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
+
+public class Client {
+    @BsonProperty("_id")
+    private ObjectId id;
+
+    @BsonProperty("clientId")
+    private int clientId;
+
+    @BsonProperty("clientName")
+    private String name;
+
+    @BsonProperty("serviceCode")
+    private int serviceCode;
+
+    // No-argument constructor
+    public Client() {}
+
+    // Parameterized constructor
+    public Client(ObjectId id, int clientId, String name, int serviceCode) {
+        this.id = id;
+        this.clientId = clientId;
+        this.name = name;
+        this.serviceCode = serviceCode;
+    }
+
+    // Getters and Setters
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getServiceCode() {
+        return serviceCode;
+    }
+
+    public void setServiceCode(int serviceCode) {
+        this.serviceCode = serviceCode;
+    }
+}
